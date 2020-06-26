413c9858469f4722f44d0c98265a58c5d15a2145
lprs110 <pro.lucasreis@gmail.com>
2020-06-26 12:57:12 -0400

    feat: add create and find routes for transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,21 @@
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface TransactionsWithBalance {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +25,49 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): TransactionsWithBalance {
+    return {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (acm: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acm.income += transaction.value;
+            break;
+          case 'outcome':
+            acm.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return acm;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
